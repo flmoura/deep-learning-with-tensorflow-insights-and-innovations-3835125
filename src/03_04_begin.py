ab170e88b6fcce3f4ed979a9ca8f1519df8344a4
DigiFab AI <digifabai@gmail.com>
2024-08-27 22:22:52 +0000

    show plot removed

--- src/03_04_begin.py
+++ src/03_04_begin.py
@@ -26,7 +26,6 @@ housing_df.describe().transpose()
 plt.figure(figsize=(10, 6))
 housing_df.hist(bins=50, figsize=(20, 15))
 plt.tight_layout()
-plt.show()
 
 # Save the distribution plot
 plt.savefig('output/03_04_housing_data_distribution.png')
